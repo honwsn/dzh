6799694a2b465b5e2727ee3a403d802789f4fdf9
honwsn <56668778@qq.com>
2014-12-17 18:01:06 +0800

    上次提交遗漏一个文件

--- src/com/hw/dzh/proto/ProtoCommandExecutor.java
+++ src/com/hw/dzh/proto/ProtoCommandExecutor.java
@@ -7,6 +7,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.hw.dzh.AppEngine;
+import com.hw.dzh.EnvironmentManager;
+import com.hw.dzh.network.BinaryRequest;
 
 public class ProtoCommandExecutor {
 
@@ -36,32 +38,31 @@ public class ProtoCommandExecutor {
 
 		mQueue.add(r);
 	}
-
+	
 	public boolean doExecute(ProtoRequest r, ProtoRequestCallBack callback) {
 		if (r == null)
 			return false;
 
-//		byte[] postData = null;
-//		postData = getPostDataFromWupRequest(r);
-//
-//		if (postData == null)
-//			return false;
-//
-//		com.hw.dzh.network.BinaryRequest request;
-//
-//		if (r.getReqUrl() != null && r.getReqUrl().length()>1){
-//			//get Server List
-//			request = new BinaryRequest(
-//					r.getReqUrl(), postData, callback, callback);
-//		}else{
-//			request = new BinaryRequest(
-//					EnvironmentManager.WUP_SERVER_ADDR, postData, callback, callback);
-//			
-//		}
-//		request.setTag(r.getId());
-//		request.setShouldCache(false);
-//		r.setRequest(request);
-//		mQueue.add(request);
+		byte[] postData = r.toByteArray();
+
+		if (postData == null)
+			return false;
+
+		com.hw.dzh.network.BinaryRequest request;
+
+		if (r.getReqUrl() != null && r.getReqUrl().length()>1){
+			//get Server List
+			request = new BinaryRequest(
+					r.getReqUrl(), postData, callback, callback);
+		}else{
+			request = new BinaryRequest(
+					EnvironmentManager.WUP_SERVER_ADDR, postData, callback, callback);
+			
+		}
+		request.setTag(r.getCommandID());
+		request.setShouldCache(false);
+		r.setRequest(request);
+		mQueue.add(request);
 
 		return true;
 	}
