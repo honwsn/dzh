253400ddc5361572a149c38c9154c4efd624e290
honwsn <56668778@qq.com>
2014-12-17 17:13:17 +0800

    添加一个Proto文件以及更新基本的命令接口

--- src/com/hw/dzh/proto/ProtoRequest.java
+++ src/com/hw/dzh/proto/ProtoRequest.java
@@ -1,14 +1,30 @@
 package com.hw.dzh.proto;
 
+import com.hw.dzh.DataCommand.Command;
+
 public class ProtoRequest {
 
 	private String mServerName = "";
 	private String mFuncName = "";
-	private String mEncodeName = "UTF-8";
-	private Object payLoad;
+	private Object mPayLoad;
+	protected Command mCommandID;
 
 	public ProtoRequest() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public ProtoRequest(String serverName, String funcName, Object payLoad) {
+		mServerName = serverName;
+		mFuncName = funcName;
+		mPayLoad = payLoad;
+	}
+
+	public void setCommandID(Command id) {
+		this.mCommandID = id;
+	}
+
+	public Command getCommandID() {
+		return mCommandID;
 	}
 
 }
